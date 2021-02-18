21b39f6c3d1c3c61ef207ad8ae763a7f65dd54b9
Calvin Stepan <calvinstepan@gmail.com>
2021-02-18 00:07:48 -0600

    Version2.0 Finished all the methods and only the javadoc for the recursion class Still need to do javadoc for the client class and clean up the interface so it loops around to ask you until you exit. Also need to make number 1 and the whole thing typesafe. look at past labs for reference.

--- Lab104-StepanCJ/src/lab104/stepancj/Client.java
+++ Lab104-StepanCJ/src/lab104/stepancj/Client.java
@@ -1,5 +1,6 @@
 package lab104.stepancj;
 
+import java.io.File;
 import javax.swing.JOptionPane;
 
 /**
@@ -13,62 +14,66 @@ public class Client {
      */
     public static void main(String[] args) {
 
-        // showInputDialogExample("Please Select the algorithm you would like to test:
-        // \n 1) nth Harmonic Number \n 2) Isabel's Technique \n 3) Print and Find",
-        // "Algorithm Selection");
-
-        showInputDialogMenuExample();
+        showInputDialogMenuExample2();
 
     }
 
-    public static void showInputDialogExample(String message, String title) {
+    public static void showInputDialogMenuExample2() {
         String response;
 
-        response = JOptionPane.showInputDialog(null, message, title, 0);
+        String optionString = "Please Select the algorithm you would like to test: \n 1) nth Harmonic Number \n 2) Isabel's Technique \n 3) Find and Print";
 
-        System.out.println("respone = " + response);
-    }
-
-    public static void showInputDialogMenuExample() {
-        String response;
-
-        String optionString = "Please Select the algorithm you would like to test: \n 1) nth Harmonic Number \n 2) Isabel's Technique \n 3) Print and Find";
-
-        response = JOptionPane.showInputDialog(null, optionString, "Algorithm Selection", 0);
+        response = JOptionPane.showInputDialog(optionString);  
         int answer = Integer.parseInt(response);
         while (!(answer > 0 && answer < 4)) {
             JOptionPane.showMessageDialog(null, "Your entry was not 1, 2, or 3, please try again");
-            response = JOptionPane.showInputDialog(null, optionString, "Algorithm Selection", 0);
+            response = JOptionPane.showInputDialog(optionString);
             answer = Integer.parseInt(response);
         }
         switch (response) {
             case "1":
                 System.out.println("Computing the nth Harmonic Number");
-                response = JOptionPane.showInputDialog(null, "Please enter a positive double value",
-                        "nth Harmonic Number", 0);
-                double option = Double.parseDouble(response);
-                while (option < 0) {
-                    JOptionPane.showMessageDialog(null, "Your entry was not valid, please try again");
-                    response = JOptionPane.showInputDialog(null, "Please enter a positive double value",
-                        "nth Harmonic Number", 0);
-                    option = Double.parseDouble(response);
+                response = JOptionPane.showInputDialog("Please enter a positive integer value");
+                int option = -1;
+                boolean flag = false;
+                try {
+                    option = Integer.parseInt(response);
+                } catch (NumberFormatException nfe) {
+                    System.err.println("error");
+                    flag = true;
                 }
-                Recursion.harmonicSetup(option);
-                JOptionPane.showMessageDialog(null, "The Answer is " + Recursion.harmonicComputation(option), "Answer", 0);
+                while (!Recursion.harmonicSetup(option) || flag == true) {
+                    JOptionPane.showMessageDialog(null, "Your entry was not valid, please try again");
+                    response = JOptionPane.showInputDialog(null, "Please enter a positive double value");
+                    option = Integer.parseInt(response);
+                }
+                JOptionPane.showMessageDialog(null, "The Answer is " + Recursion.harmonicComputation(option));
                 break;
             case "2":
                 System.out.println("Using Isabel's Technique");
-                response = JOptionPane.showInputDialog(null, "Please enter a file path", "Size", 0);
-                int[] A = Recursion.isabelsTechniqueSetup(response); 
+                response = JOptionPane.showInputDialog(null, "Please enter a file path");
+                File isabelPath = new File(response);
+                while (!isabelPath.isFile()) {
+                    JOptionPane.showMessageDialog(null, "Your file path was not valid, please try again");
+                    response = JOptionPane.showInputDialog(null, "Please enter a file path");
+                    isabelPath = new File(response);
+                }
+                int[] A = Recursion.isabelsTechniqueSetup(response);
                 JOptionPane.showMessageDialog(null, "The summation of the array is " + Recursion.isabelsTechnique(A));
                 break;
             case "3":
+                System.out.println("Finding and Printing");
                 String pathName = JOptionPane.showInputDialog("Please enter the start path");
                 String fileName = JOptionPane.showInputDialog("Please enter the file name");
+                File findPath = new File(pathName);
+                while (!findPath.isDirectory()) {
+                    JOptionPane.showMessageDialog(null, "Invalid input, please try again");
+                    pathName = JOptionPane.showInputDialog("Please enter the start path");
+                    fileName = JOptionPane.showInputDialog("Please enter the file name");
+                }
                 Recursion.find(pathName, fileName);
-                System.out.println("Printing and Finding");
             default:
-                System.err.println("invalid entry");
+            //JOptionPane.showInputDialog(response)
         }
     }
     //C:\\Users\\Calvin\\Downloads\\ab.txt
--- Lab104-StepanCJ/src/lab104/stepancj/Recursion.java
+++ Lab104-StepanCJ/src/lab104/stepancj/Recursion.java
@@ -3,27 +3,50 @@ package lab104.stepancj;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+import javax.swing.JOptionPane;
 
 /**
- *
+ * The Recursion class uses recursion to compute algorithms and complete file searches within directories
+ * I got some help on various parts of this class from Qianqian, the TA. Those parts are denoted by comments or noted within the javadoc header for that method
+ * The basis for my find() method came from Stack Overflow (https://stackoverflow.com/questions/2534632/list-all-files-from-a-directory-recursively-with-java)
+ * 
+ * Transcribed by
  * @author Calvin Stepan
+ * @version 2.17.2021
  */
 public class Recursion {
 
-    public static void harmonicSetup(double n) {
-        if (n < 0) {
-            System.err.print("Value for n is invalid, would you like to try again?");
-        }
+    /**
+     * Setup for the harmonicComputation method
+     * Only needs to run once to verify the input will work for the method, then passes the value to the recursive method
+     * @param n a positive integer representing what harmonic number to compute
+     */
+    public static boolean harmonicSetup(int n) {
+        if (n < 0) 
+            return false;
+        else 
+            return true;
     }
 
-    public static double harmonicComputation(double n) {
+    /**
+     * Recursive method to compute the nth harmonic number
+     * @param n a positive integer value
+     * @return the double value of the nth harmonic number
+     */
+    public static double harmonicComputation(int n) {
         if (n == 1) {
-            return 1.0;
+            return 1;
         } else {
-            return ((1.0 / n) + (harmonicComputation(n - 1.0)));
+            return ((1.0 / n) + (harmonicComputation(n - 1)));
         }
     }
 
+    /**
+     * This method came from Qianqian during her office hours
+     * Separate method to determine if the size of the array is a power of 2
+     * @param n the size of the array;
+     * @return true if the size is a power of 2, else false
+     */
     public static boolean isPowerOfTwo(int n) {
         if (n == 0) {
             return false;
@@ -38,24 +61,29 @@ public class Recursion {
         return true;
     }
 
+    /**
+     * This is the setup method for Isabel's Technique. It determines if the file is valid and if it is a power of two.
+     * After checking for validity, it parses the file and puts the contents into an int[] array to be returned.
+     * @param filePath the String representing the path of the file to be scanned
+     * @return an int[] array with all the integers from the parsed file
+     */
     public static int[] isabelsTechniqueSetup(String filePath) {
-        Scanner scan = null;
         ArrayBag<Integer> Bag = new ArrayBag<>();
         int[] A = new int[2];
 
         try {
-            scan = new Scanner(new File(filePath));
+            Scanner scan = new Scanner(new File(filePath));
+            while (scan.hasNext()) {
+                String token = scan.next();
+
+                try {
+                    int inInt = Integer.parseInt(token);
+                    Bag.add(inInt);
+                } catch (NumberFormatException nfe) {
+                }
+            }
         } catch (FileNotFoundException fnfe) {
             System.err.println("Invalid file path");
-        }
-        while (scan.hasNext()) {
-            String token = scan.next();
-
-            try {
-                int inInt = Integer.parseInt(token);
-                Bag.add(inInt);
-            } catch (NumberFormatException nfe) {
-            }
         }
 
         //Power of 2 check
@@ -66,19 +94,18 @@ public class Recursion {
                 A[i] = Bag.get(i);
             }
         } else {
-            System.err.println("The number of elements in the text file is not a power of two");
+            throw new IllegalStateException("array length is not a power of 2");
         }
-
         return A;
     }
 
-    public static int isabelsTechnique(int[] A) throws IllegalArgumentException {
-        /**
-         * 1) open the ASCII text file 2) scan the contents of the document and
-         * put the values into an array A 3) create an array B that is half the
-         * size of A ASCII File is just a text file
-         *
-         */
+    /**
+     * Uses Isabel's Technique to sum all the integers in an array
+     * @param A the int[] array from the setup method
+     * @return the sum of all the integers in the array
+     */
+    public static int isabelsTechnique(int[] A) {
+
         // base case
         if (A.length == 1) {
             return A[0];
@@ -93,26 +120,24 @@ public class Recursion {
 
             return isabelsTechnique(arrayB);
         }
-
-        /*
-        int[] B = new int[A.length / 2];
-        for (int i = 0; i < ((A.length / 2) - 1); i++) {
-            B[i] = A[2 * i] + A[(2 * i) + 1];
-
-        }
-        if (B.length == 1) {
-            return B[0];
-        } else {
-            return isabelsTechnique(B);
-        }
-         */
-    }
-    
-    public static void find(String startPath, String filename) {
-        File path = new File(startPath);
-        if(path != null && path.isFile()) {
-            
-        }
     }
 
+    /**
+     * This method was taken from Stack Overflow (https://stackoverflow.com/questions/2534632/list-all-files-from-a-directory-recursively-with-java)
+     * Given a starting directory and a file name, parse the directory for any files matching the given name
+     * @param startPath the starting directory to begin the file search from 
+     * @param fileName the file name to search the directory for
+     */
+    public static void find(String startPath, String fileName) {
+        File[] fileList = new File(startPath).listFiles();
+        for (File file : fileList) {
+            if (file.getName().matches(fileName)) {
+                System.out.println(file.getAbsolutePath());
+                JOptionPane.showMessageDialog(null, "Matching file found at " + file.getAbsolutePath());
+            }
+            if (file.isDirectory()) {
+                find(file.getAbsolutePath(), fileName);
+            }
+        }
+    }
 }
