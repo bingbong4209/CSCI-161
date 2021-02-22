d5262199d1565db5b6e471ddda2d6345516ae4e2
Calvin Stepan <calvinstepan@gmail.com>
2021-02-21 22:40:15 -0600

    Version 1.0 Initial commit of Lab 105

---
+++ Lab105-StepanCJ/src/lab105/stepancj/Queue.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab105.stepancj;
+
+/**
+ *
+ * @author Calvin Stepan
+ * @param <E>
+ */
+public interface Queue<E> {
+ //Returns the number of elements in the queue. ∗/
+ int size( );
+ //Tests whether the queue is empty. ∗/
+ boolean isEmpty( );
+ //Inserts an element at the rear of the queue. ∗/
+ void enqueue(E e);
+ //Returns, but does not remove, the first element of the queue (null if empty). ∗/
+ E first( );
+ //Removes and returns the first element of the queue (null if empty). ∗/
+ E dequeue( );
+ }
