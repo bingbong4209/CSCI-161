bdb08a9f21dc1df33069b4de6b9c0cb6cea72b4e
Calvin Stepan <calvinstepan@gmail.com>
2021-02-23 16:50:10 -0600

    Version 1.25 Working on formatting the ASCII table, should finish it tonight

--- Lab105-StepanCJ/src/lab105/stepancj/Client.java
+++ Lab105-StepanCJ/src/lab105/stepancj/Client.java
@@ -6,5 +6,30 @@ package lab105.stepancj;
  * @version 2.23.2021
  */
 public class Client {
-    
+
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 23;
+        double c = 12.34;
+        int n = 1000000;
+
+        int lineLength = 20;
+        int columns = 2;
+        String lineString = "";
+        String junctions = "+";
+        
+        System.out.printf("| %,9d  |", a);
+        System.out.printf("| %,9d  |\t\n", a);
+        System.out.printf("| %,9d  |", b);
+        System.out.printf("| %,9d  |\t\n", b);
+        System.out.printf("| %,9.2f  |", c);
+        System.out.printf("| %,9.2f  |\t\n", c);
+        System.out.printf("| %,9d  |", n);
+        System.out.printf("| %,9d  |\t\n", n);
+
+        for (int i = 0; i < lineLength / columns; i++) {
+            lineString += "-";
+        }
+        System.out.printf(lineString + junctions);
+    }
 }
