7f58a69df65e6a75514869972baacf90f346270a
Calvin Stepan <calvinstepan@gmail.com>
2021-02-22 21:33:47 -0600

    fixed the errors in the code for the ArrayList class

--- Lab105-StepanCJ/src/lab105/stepancj/ArrayList.java
+++ Lab105-StepanCJ/src/lab105/stepancj/ArrayList.java
@@ -51,28 +51,14 @@ public class ArrayList<E> implements List<E> {
     }
     // Inserts element e to be at index i, shifting all subsequent elements later. ∗/
 
-    @Override
-    public void add(int i, E e) throws IndexOutOfBoundsException,IllegalStateException {
-        checkIndex(i, size + 1);
-        if (size == data.length) // not enough capacity
-        {
-            throw new IllegalStateException("Array is full");
-        }
-        for (int k = size - 1; k >= i; k--) {
-            data[k + 1] = data[k];
-            data[i] = e; // ready to place the new element
-            size++;
-        }
-    }//Removes/returns the element at index i, shifting subsequent elements earlier. ∗/
-
     public E remove(int i) throws IndexOutOfBoundsException {
         checkIndex(i, size);
         E temp = data[i];
-        for (int k = i; k < (size − 1); k++) { // shift elements to fill hole
-        data[k] = data[k + 1];
-        data[size − 1] = null; // help garbage collection
-        size--;
-    }
+        for (int k = 0; k < size - 1; k++) {
+            data[k] = data[k + 1];
+            data[size - 1] = null;
+            size--;
+        }
         return temp;
     }
 
@@ -85,20 +71,20 @@ public class ArrayList<E> implements List<E> {
     }
 
 //∗ Resizes internal array to have given capacity >= size. ∗/
-protected void resize(int capacity) {
-    E[ ] temp = (E[ ]) new Object[capacity]; // safe cast; compiler may give warning
-    for (int k=0; k < size; k++)
-    temp[k] = data[k];
-    data = temp; // start using the new array
+    protected void resize(int capacity) {
+        E[] temp = (E[]) new Object[capacity]; // safe cast; compiler may give warning
+        for (int k = 0; k < size; k++) {
+            temp[k] = data[k];
+        }
+        data = temp; // start using the new array
     }
 
-
     //Inserts element e to be at index i, shifting all subsequent elements later. ∗/
-     public void add(int i, E e) throws IndexOutOfBoundsException {
-     checkIndex(i, size + 1);
-     if (size == data.length) // not enough capacity
-     resize(2 ∗ data.length); // so double the current capacity
-     // rest of method unchanged...
+    public void add(int i, E e) throws IndexOutOfBoundsException {
+        checkIndex(i, size + 1);
+        if (size == data.length) {
+            throw new IllegalStateException("Array is full");
+        }// not enough capacity
+        resize(2 * data.length); // so double the current capacity
+    }
 }
-}
-
---
+++ Lab105-StepanCJ/src/lab105/stepancj/Client.java
@@ -0,0 +1,10 @@
+package lab105.stepancj;
+
+/**
+ *
+ * @author Calvin Stepan
+ * @version 2.22.2021
+ */
+public class Client {
+    
+}
