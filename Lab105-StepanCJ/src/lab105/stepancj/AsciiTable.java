264844daf1592d795aa5ef7ef97e9e139bfb07da
Calvin Stepan <calvinstepan@gmail.com>
2021-02-27 23:00:35 -0600

    can't figure out what is wrong, gonna finish on monday

--- Lab105-StepanCJ/src/lab105/stepancj/AsciiTable.java
+++ Lab105-StepanCJ/src/lab105/stepancj/AsciiTable.java
@@ -12,13 +12,7 @@ package lab105.stepancj;
 public class AsciiTable {
 
     public static void asciiOutput(long[][] array) {
-        //int commaCount = 0;
-        //int negative = 0;
-        //long smallestValue = 0;
-        //int spaces;
         int capacity = array[0].length;//tells us how many cells are in the first column of our array
-        //long largestValue = 0;
-
         int[] columnWidths = new int[capacity];
 
         String result;
@@ -28,12 +22,6 @@ public class AsciiTable {
                 if (columnWidths[col] < result.length()) {
                     columnWidths[col] = result.length();
                 }
-                /*
-                if(array[j][i] > largestValue)
-                    largestValue = array[j][i];
-                if(array[j][i] < smallestValue)
-                    smallestValue = array[j][i];
-                 */
             }
         }
         
@@ -59,43 +47,6 @@ public class AsciiTable {
                 }
             }
         }
-        System.out.printf("%n" + lineString + "%n");
-        /*
-        String largestInt = "" + largestValue;
-        String smallestInt = "" + smallestValue;
-        String value = largestInt;
-        if(largestInt.length() < smallestInt.length())
-            value = smallestInt;
-        
-        if(value.length() > 3 && value.length() < 7)
-            commaCount = 1;
-        else if(value.length() > 6 && value.length() < 10)
-            commaCount = 2;
-        else if(value.length() > 9 && value.length() < 13)
-            commaCount = 3;
-        else if(value.length() > 12 && value.length() < 16)
-            commaCount = 4;
-        if(smallestValue < 0)
-            negative = 1;
-         */
-
-        /*spaces = value.length() + commaCount + negative;
-        int lineLength = spaces + 4;
-        
-        String tableFormat = "";
-        for(int i = 0; i < lineLength; i++)
-            lineString += "-";
-        
-        for (int i = 0; i < capacity; i++) {
-            tableFormat += lineString;
-            if(i == capacity - 1)
-                tableFormat += "+";
-        }     
-         */
-        
+        System.out.printf("%n" + lineString + "%n");   
     }
-
-//    public static void addTime(int index1, int index2, long time, long[][] array) {
-//        array[index1][index2] = time;
-//    }
 }
--- Lab105-StepanCJ/src/lab105/stepancj/Client.java
+++ Lab105-StepanCJ/src/lab105/stepancj/Client.java
@@ -13,8 +13,8 @@ public class Client {
 
     public static void main(String[] args) {
         
-        int maxN = 1000000000;
-        int capacity = 6;
+        int maxN = 100000000;
+        int capacity = 8;
         long[][] times = new long[capacity][capacity];
 
         int row = 0;
@@ -27,32 +27,7 @@ public class Client {
             times[row][5] = testArrayList(i);
             row++;
         }
-
         AsciiTable.asciiOutput(times);
-        /*
-        long startTime = 0;
-        long stopTime = 0;
-        long totalTime = 0;
-
-        /*
-        while (n < 100) {
-            for (int i = 0; i < capacity; i++) {
-                for (int j = 0; j < capacity; j++) {
-                    startTime = System.nanoTime();
-                    for (int k = 0; k < n; k++) {
-                        arrayStack.push(10);
-                        arrayStack.pop();
-                    }
-                    stopTime = System.nanoTime();
-                    totalTime = stopTime - startTime;
-
-                    n = (n * 10);
-                    AsciiTable.addTime(j, i, totalTime, times);
-                }
-            }
-
-        }
-         */
     }
 
     public static long testArrayStack(int n) {
@@ -116,15 +91,13 @@ public class Client {
     }
 
     public static long testArrayList(int n) {
-        ArrayList<Integer> list = new ArrayList<>();
+        ArrayList<Integer> list = new ArrayList<>(n);
         long start = System.nanoTime();
-        for (int i = 0; i < list.size(); i++) {
+        for (int i = 0; i < n; i++) {
             list.add(i, rand.nextInt());
-            System.out.println("made it");
         }
-        for (int i = 0; i < list.size(); i++) {
-            list.remove(i);
-            System.out.println("made it");
+        for (int i = 0; i < n; i++) {
+            list.remove(n-i-1);
         }
         long stop = System.nanoTime();
         long total = stop - start;
