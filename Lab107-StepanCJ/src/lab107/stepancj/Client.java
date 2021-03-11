25b782452a6b5f2a143c89f8c7bf508315662e94
Calvin Stepan <calvinstepan@gmail.com>
2021-03-10 18:03:10 -0600

    got a start on the prints for the binary tree

--- Lab107-StepanCJ/src/lab107/stepancj/Client.java
+++ Lab107-StepanCJ/src/lab107/stepancj/Client.java
@@ -1,9 +1,12 @@
 package lab107.stepancj;
 
+import java.util.Iterator;
+
 /**
  * The client class holds the binary tree experiment code
  * We start by creating an instance of a binary tree and traversing it to test our methods
  * @author Calvin Stepan
+ * @version 3.10.2021
  */
 public class Client {
 
@@ -21,6 +24,7 @@ public class Client {
         leftAdd.addRoot("+");
         leftAdd.addLeft(nine.root(), nine);
         leftAdd.addRight(two.root(), two);
+        
         
         //Subtree 2 on the right(3 * 8)
         LinkedBinaryTree three = new LinkedBinaryTree();
@@ -47,6 +51,22 @@ public class Client {
         finalTree.addRight(minus.root(), minus);
                 
         System.out.println("(   2   +   9   )   +   (   7   -   (   3    *   8   )   )");
+        
+        Iterator<Position<String>> preorderTraversal = finalTree.preorder().iterator();
+        Iterator<Position<String>> postorderTraversal = finalTree.postorder().iterator();
+        Iterator<Position<String>> inorderTraversal = finalTree.inorder().iterator();
+        Iterator<Position<String>> breathfirstTraversal = finalTree.inorder().iterator();
+        //while(preorderTraversal.hasNext())
+            System.out.println(preorderTraversal.next());
+            System.out.println(finalTree.size());
+        
+        //System.out.println(finalTree.height(finalTree.root()));
+        
+        /**
+         * create iterator for each traversal
+         * while has next loop
+         * print out contents
+         */
         /**
          * Print out the following:
          * height of tree
