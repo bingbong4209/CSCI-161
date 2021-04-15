d32c216119cc9ad64b1fb0f35b65c2fb3f7270e2
Calvin Stepan <calvinstepan@gmail.com>
2021-04-14 22:07:10 -0500

    Lab110 v0.5 Initial commit of Lab110 before it is even released (kinda risky but I should be ok)

---
+++ Lab110-StepanCJ/src/lab110/stepancj/Lab110StepanCJ.java
@@ -0,0 +1,16 @@
+package lab110.stepancj;
+
+/**
+ *
+ * @author Calvin Stepan
+ */
+public class Lab110StepanCJ {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
