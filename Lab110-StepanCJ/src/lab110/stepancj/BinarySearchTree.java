e38107a23debeffe9361a94734ba99162ce32c06
Calvin Stepan <calvinstepan@gmail.com>
2021-04-20 22:01:47 -0500

    Lab110 v1.1 Made some real progress. Need to test to see if I did things right, otherwise I may be finished

--- Lab110-StepanCJ/src/lab110/stepancj/BinarySearchTree.java
+++ Lab110-StepanCJ/src/lab110/stepancj/BinarySearchTree.java
@@ -4,16 +4,18 @@ package lab110.stepancj;
  *
  * @author Calvin Stepan
  * @param <E> generic type E
+ * @version 4.20.2021
  */
 public class BinarySearchTree<E> extends AbstractBinaryTree<E> {
 //Represent a node of binary tree  
 
-    private static class Node<E> {
+    private static class Node<E> implements Position<E>{
 
         private int data;
         private Node left;
         private Node right;
         private Node parent;
+        private E element;
 
         private Node(int data) {
             //Assign data to the new node, set left and right children to null  
@@ -21,13 +23,61 @@ public class BinarySearchTree<E> extends AbstractBinaryTree<E> {
             this.left = null;
             this.right = null;
         }
+
+        public int getData() {
+            return data;
+        }
+        
+        public Node getLeft() {
+            return left;
+        }
+        
+        public Node getRight() {
+            return right;
+        }
+        
+        public Node getParent() {
+            return parent;
+        }
+        
+        public void setData(int data) {
+            this.data = data;
+        }
+        
+        public void setLeft(Node left) {
+            this.left = left;
+        }
+        
+        public void setRight(Node right) {
+            this.right = right;
+        }
+        
+        public void setParent(Node parent) {
+            this.parent = parent;
+        }
+        
+        @Override
+        public E getElement() {
+            return element;
+        }
     }
 
     //Represent the root of binary tree  
     private Node root;
+    private final int size = 0;
 
+    @Override
+    public Position<E> root() {
+        return root;
+    }
+    
     public BinarySearchTree() {
         root = null;
+    }
+    
+    @Override
+    public int size() {
+        return size;
     }
 
     //insert() will add new node to the binary search tree  
@@ -40,7 +90,7 @@ public class BinarySearchTree<E> extends AbstractBinaryTree<E> {
             root = newNode;
         } else {
             //current node point to root of the tree  
-            Node current = root, parent = null;
+            Node current = root, parent;
 
             while (true) {
                 //parent keep track of the parent node of current node.  
@@ -127,5 +177,36 @@ public class BinarySearchTree<E> extends AbstractBinaryTree<E> {
             }
 
         }
+    }
+    
+    //Returns the Position of p's parent (or null if p is root)
+    @Override
+    public Position<E> parent(Position<E> p) throws IllegalArgumentException {
+        Node<E> node = validate(p);
+        return node.getParent();
+    }
+    
+    //Returns the Position of p's left child (or null if no child exists)
+    @Override
+    public Position<E> left(Position<E> p) throws IllegalArgumentException {
+        Node<E> node = validate(p);
+        return node.getLeft();
+    }
+    
+    //Returns the Position of p's right child (or null if no child exists)
+    @Override
+    public Position<E> right(Position<E> p) throws IllegalArgumentException {
+        Node<E> node = validate(p);
+        return node.getRight();
+    }
+    
+    //Validates the position and returns it as a node
+    protected Node<E> validate(Position<E> p) throws IllegalArgumentException {
+        if(!(p instanceof Node))
+            throw new IllegalArgumentException("Not valid position type");
+        Node<E> node = (Node<E>)p;          //safe cast
+        if(node.getParent() == node)
+            throw new IllegalArgumentException("p is no longer in the tree");
+        return node;
     }
 }
--- Lab110-StepanCJ/src/lab110/stepancj/Client.java
+++ Lab110-StepanCJ/src/lab110/stepancj/Client.java
@@ -1,10 +1,9 @@
 package lab110.stepancj;
 
-import lab110.stepancj.BinarySearchTree.Node;
-
 /**
  *
  * @author Calvin Stepan
+ * @version 4.20.2021
  */
 public class Client {
 
@@ -12,34 +11,7 @@ public class Client {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-         BinarySearchTree bt = new BinarySearchTree();  
-          //Add nodes to the binary tree  
-          bt.insert(50);  
-          bt.insert(30);  
-          bt.insert(70);  
-          bt.insert(60);  
-          bt.insert(10);  
-          bt.insert(90);  
-  
-          System.out.println("Binary search tree after insertion:");  
-          //Displays the binary tree  
-          bt.inorderTraversal(bt.root);  
-  
-          Node deletedNode = null;  
-          //Deletes node 90 which has no child  
-          deletedNode = bt.deleteNode(bt.root, 90);  
-          System.out.println("\nBinary search tree after deleting node 90:");  
-          bt.inorderTraversal(bt.root);  
-  
-          //Deletes node 30 which has one child  
-          deletedNode = bt.deleteNode(bt.root, 30);  
-          System.out.println("\nBinary search tree after deleting node 30:");  
-          bt.inorderTraversal(bt.root);  
-  
-          //Deletes node 50 which has two children  
-          deletedNode = bt.deleteNode(bt.root, 50);  
-          System.out.println("\nBinary search tree after deleting node 50:");  
-          bt.inorderTraversal(bt.root);  
+         
     }
     
 }
