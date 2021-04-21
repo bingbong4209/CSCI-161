fc1c73716113a92f91011cc8681b743f27cf84a7
Calvin Stepan <calvinstepan@gmail.com>
2021-04-20 23:25:06 -0500

    Lab110 v1.11 more cleanup work

--- Lab110-StepanCJ/src/lab110/stepancj/Client.java
+++ Lab110-StepanCJ/src/lab110/stepancj/Client.java
@@ -11,6 +11,14 @@ public class Client {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+         BinarySearchTree tree = new BinarySearchTree();
+         
+         for(int i = 0; i < 10000; i++) {
+             tree.insert(i + 1);
+             if(i % 1000 == 0 && i != 0)
+                System.out.println(i + " ");
+         }
+         System.out.println("\n" + tree.height(tree.root()));
          
     }
     
