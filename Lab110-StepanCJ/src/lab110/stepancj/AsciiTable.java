e95b4098cb103108e1e8c18b9ac454922c92265a
Calvin Stepan <calvinstepan@gmail.com>
2021-04-22 00:50:09 -0500

    Lab110 v1.12 Finished more framework on the client. Still need to figure out the best way to avoid collisions with random numbers, maybe look back at lab109 to evaluate collisions, idk

---
+++ Lab110-StepanCJ/src/lab110/stepancj/AsciiTable.java
@@ -0,0 +1,112 @@
+package lab110.stepancj;
+
+import javax.swing.JOptionPane;
+
+/**
+ * This is the class where my Ascii Table code is kept. I use a static method so
+ * you only need to import the class, not create an instance of it.
+ *
+ * @author Calvin Stepan
+ * @version 4.10.2021
+ */
+public class AsciiTable {
+
+    /**
+     *
+     * @param array a long[][] array representing run times in nanoseconds for
+     * various structures
+     * @param tableHeader the String representing the table header
+     */
+    public static void asciiOutput(long[][] array, String tableHeader) {
+        int columnCount = array[0].length;//tells us how many cells are in the first column of our array
+        int rowCapacity = array.length;
+        int[] columnWidths = new int[columnCount];
+
+        //parse each column to find out how wide each needs to be
+        String result;
+        for (int row = 0; row < rowCapacity; row++) {
+            for (int col = 0; col < columnCount; col++) {
+                result = String.format("%,d", array[row][col]);
+                if (columnWidths[col] < result.length()) {
+                    columnWidths[col] = result.length();
+                }
+            }
+        }
+
+        //use column data to construct ascii table lines
+        String lineString = "";
+        for (int row = 0; row < columnWidths.length; row++) {
+            if (row == 0) {
+                lineString = "+";
+            }
+            for (int i = 0; i < columnWidths[row]; i++) {
+                lineString += "-";
+            }
+            lineString += "----";
+            lineString += "+";
+        }
+
+        //loop to make the top line of the table
+        String topLine = "";
+        for(int j = 0; j < lineString.length(); j++) {
+            if(j == 0 || j == lineString.length() - 1)
+                topLine += "+";
+            else
+                topLine += "-";
+        }
+        
+        //--------Writing Table Headers and Column Headers--------
+        //variables to attempt to center the table header
+        int tableWidthLeft = ((lineString.length() - columnCount - 1) / 2) + 5;
+        int tableWidthRight = ((lineString.length() - columnCount - 1) / 2) - 5;
+
+        //print out Table Header
+        System.out.printf("%n" + topLine + "%n");
+        System.out.printf("| %" + tableWidthLeft + "s" + "%" + tableWidthRight + "s |", tableHeader, "");
+        System.out.printf("%n" + lineString + "%n");
+        
+        //loop to print out the columnHeaders
+        for (int c = 0; c < columnCount; c++) {
+            //uses user input to create the headers
+            /*int count = c + 1;
+            String columnHeader = JOptionPane.showInputDialog("Please Enter a Header For Column " + count);
+            if(columnHeader.length() > 8) {
+                throw new IllegalArgumentException("header was too long");
+            }
+            
+            if (c == 0) {
+                System.out.printf("| %-" + columnWidths[c] + "s |", columnHeader);
+            } else if (c == columnCount - 1) {
+                System.out.printf(" %-" + columnWidths[c] + "s |", columnHeader);
+            } else {
+                System.out.printf(" %-" + columnWidths[c] + "s |", columnHeader);
+            }
+            */
+            //predefined column headers
+            int width = columnWidths[c] + 4;
+            switch(c) {
+                case 0:
+                    System.out.printf("|%-" + width + "s|", "a/p");
+                    break;
+                case 1:
+                    System.out.printf("%-" + width + "s|", "total");
+                    break;
+                case 2:
+                    System.out.printf("%-" + width + "s|", "max");
+                    break;
+            }
+        }
+        //print out the values for each test within the ascii table
+        for (int row = 0; row < rowCapacity; row++) {
+            System.out.printf("%n" + lineString + "%n");
+            for (int col = 0; col < columnCount; col++) {
+                if (col == 0) {
+                    System.out.printf("|  %," + columnWidths[col] + "d  |", array[row][col]);
+                } else {
+                    System.out.printf("  %," + columnWidths[col] + "d  |", array[row][col]);
+                }
+            }
+        }
+        System.out.printf("%n" + lineString + "%n");
+    }
+}
--- Lab110-StepanCJ/src/lab110/stepancj/Client.java
+++ Lab110-StepanCJ/src/lab110/stepancj/Client.java
@@ -1,5 +1,7 @@
 package lab110.stepancj;
 
+import java.util.Random;
+
 /**
  *
  * @author Calvin Stepan
@@ -11,15 +13,49 @@ public class Client {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-         BinarySearchTree tree = new BinarySearchTree();
-         
-         for(int i = 0; i < 10000; i++) {
-             tree.insert(i + 1);
-             if(i % 1000 == 0 && i != 0)
+        Random rand = new Random();
+        int[] duplicates = new int[10000];
+        /*
+        //ascending tree
+        BinarySearchTree ascendingTree = new BinarySearchTree();
+
+        for (int i = 0; i < 10000; i++) {
+            ascendingTree.insert(i + 1);
+            if (i % 1000 == 0 && i != 0) {
                 System.out.println(i + " ");
-         }
-         System.out.println("\n" + tree.height(tree.root()));
-         
+            }
+        }
+        System.out.println("\n" + ascendingTree.height(ascendingTree.root()));
+
+        //descending tree
+        BinarySearchTree descendingTree = new BinarySearchTree();
+
+        for (int i = 10000; i > 0; i--) {
+            descendingTree.insert(i);
+            if (i % 1000 == 0 && i != 0) {
+                System.out.println(i + " ");
+            }
+        }
+        System.out.println("\n" + descendingTree.height(descendingTree.root()));
+
+        //random order tree 1*/
+        BinarySearchTree randomTree1 = new BinarySearchTree();
+
+        for (int i = 10000; i > 0; i--) {
+            int number = rand.nextInt(i);
+            //if the number has already been passed to the array, we iterate until we get a good value
+            while(duplicates[number] == number) {
+                number = rand.nextInt(i);
+                System.out.println("Collision at " + i);
+            }
+            duplicates[number] = number;
+            
+            randomTree1.insert(number);
+            if (i % 1000 == 0 && i != 0) {
+                System.out.println(i + " ");
+            }
+        }
+        System.out.println("\n" + randomTree1.height(randomTree1.root()));
     }
-    
+
 }
