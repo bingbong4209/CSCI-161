6e2f368564d911897a6089a2c1d2852c5ea869c2
Calvin Stepan <calvinstepan@gmail.com>
2021-04-23 17:25:10 -0500

    more comments on what to do

--- Lab110-StepanCJ/src/lab110/stepancj/Client.java
+++ Lab110-StepanCJ/src/lab110/stepancj/Client.java
@@ -13,14 +13,28 @@ public class Client {
     public static void main(String[] args) {
         int setIterations = 8000;
         int randomIterations = 100000;
-        long[][] dataTable = new long[5][3];
-        
+        long[][] dataTable = new long[5][2];
+
         BinarySearchTree.ascendingTree(setIterations);
-        
+
         BinarySearchTree.descendingTree(setIterations);
-        
-        for(int i = 0; i < 5; i++)
-            BinarySearchTree.randomTree(randomIterations); 
+
+        for (int i = 0; i < 5; i++) {
+            BinarySearchTree.randomTree(randomIterations);
+        }
     }
+    /**
+     * ----Ascii table values---- 
+     * number of items 
+     * height of tree 
+     * do the random iterations at 100000
+     * 
+     * //probably gonna be on the test
+     * in java, are parameters passed by value or reference
+     * does it matter if they are primitives or objects
+     * write a java code that illustrates that answer
+     * 
+     * for lab111, make sure to pass in a copy of the array for sorting methods/algorithms and not pass in the original array
+     */
 
 }
